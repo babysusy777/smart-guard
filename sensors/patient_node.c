24351a88d46a1eca7ba20176dde8623646473fd1
Susanna Baldo <154971527+babysusy777@users.noreply.github.com>
2026-06-22 10:57:06 +0200

    ...

--- sensors/patient_node.c
+++ sensors/patient_node.c
@@ -228,7 +228,7 @@ void set_publish_period(int seconds) {
 //notifies Cloud App that the alarm has been resolved so that the application closes the active record in alarms
 //used by the patient to notify that he/she is ok 
 //used also by the caregiver that has served the alarm request
-static void publish_alarm_resolved(void) {
+static uint8_t publish_alarm_resolved(void) {
   snprintf(app_buffer, APP_BUFFER_SIZE, "{\"node_id\":\"%s\",\"event\":\"RESOLVED\"}", client_id);
   mqtt_publish(&conn, NULL, alarm_topic, (uint8_t *)app_buffer, strlen(app_buffer), MQTT_QOS_LEVEL_1, MQTT_RETAIN_OFF);
   return (st == MQTT_STATUS_OK) ? 1 : 0;
