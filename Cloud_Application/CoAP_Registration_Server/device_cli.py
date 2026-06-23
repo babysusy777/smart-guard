4669da134f1dbfcc788191a82368ef51ec01f8a2
Susanna Baldo <154971527+babysusy777@users.noreply.github.com>
2026-06-23 08:49:38 +0200

    ...

--- Cloud_Application/CoAP_Registration_Server/device_cli.py
+++ Cloud_Application/CoAP_Registration_Server/device_cli.py
@@ -9,7 +9,7 @@ from influxdb_client import InfluxDBClient
 
 
 INFLUX_URL = os.getenv("INFLUXDB_URL", "http://localhost:8086")
-INFLUX_TOKEN = os.getenv("INFLUXDB_TOKEN")
+INFLUX_TOKEN = os.getenv("INFLUXDB_TOKEN") 
 INFLUX_ORG = os.getenv("INFLUXDB_ORG", "myorg")
 INFLUX_BUCKET = os.getenv("INFLUXDB_BUCKET", "iot_health")
 
